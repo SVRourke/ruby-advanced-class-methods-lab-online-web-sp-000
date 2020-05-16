187f00c0e4a05d9c8c6b621ff90848bedd7a1fcc
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-16 13:49:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,14 @@ class Song
     end
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end 
+
 end
 
 a = Song.new_by_name("Blank Space")
